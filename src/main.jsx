97d8355519d9894e1969f546b6d47ff29e5ec09a
Harini <harinivelraj@gmail.com>
2025-08-31 08:07:33 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,18 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
+
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-// Removed global axios defaults to use custom axios instance in src/api/axios.js
-=======
->>>>>>> 3f014bd22e10e37ea0a98bd114216001af0af8e7
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-<<<<<<< HEAD
-// Removed global axios defaults to use custom axios instance in src/api/axios.js
-=======
-import axios from "axios";
 
-axios.defaults.baseURL="http://localhost:5000";
-axios.defaults.withCredentials=false;
->>>>>>> d3d77a7581ca8f69f49219777c1d6dc1b188395e
->>>>>>> 3f014bd22e10e37ea0a98bd114216001af0af8e7
+// If you want to set axios defaults globally, uncomment and edit below:
+// import axios from "axios";
+// axios.defaults.baseURL = "http://localhost:5000";
+// axios.defaults.withCredentials = false;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-<<<<<<< HEAD
-)
-=======
-<<<<<<< HEAD
+  </StrictMode>
 );
-=======
-)
->>>>>>> d3d77a7581ca8f69f49219777c1d6dc1b188395e
->>>>>>> 3f014bd22e10e37ea0a98bd114216001af0af8e7
